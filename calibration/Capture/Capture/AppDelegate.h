df234890363891806bb04492a7b6bbad090a0657
Peyton Randolph <pjrandolph@gmail.com>
2015-08-08 14:07:11 -0700

    Add Capture project from template

---
+++ calibration/Capture/Capture/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Capture
+//
+//  Created by Peyton Randolph on 8/8/15.
+//  Copyright © 2015 peytn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ calibration/Capture/Capture/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Capture
+//
+//  Created by Peyton Randolph on 8/8/15.
+//  Copyright © 2015 peytn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
